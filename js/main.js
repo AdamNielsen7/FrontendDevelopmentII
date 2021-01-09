8bd9257af18de57516d917ffb5eb8470a4293ce9
AdamNielsen7 <56274811+AdamNielsen7@users.noreply.github.com>
2021-01-09 15:19:41 -0700

    Week 1 notes and code

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const contents = [
+    {
+        label: "Week1 notes",
+        url: "week1/index.html"
+    }
+]
+
+
+function makeList(contents) {
+    contentsList = document.getElementById("contentsList");
+    // remove each element in list
+    while(contentsList.firstChild) contentsList.removeChild(contentsList.firstChild);
+    // add each element in contents to list
+    for (i=0; i < contents.length; i++) {
+        listItem = document.createElement('li');
+        listItem.innerHTML = "<a href='" + contents[i]["url"] + "'>" + contents[i]["label"] + "</a>";
+        contentsList.appendChild(listItem);
+    }
+}
