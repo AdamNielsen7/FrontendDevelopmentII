0b6a1e7b28b6a4e474a4c8e8fa07921332eeb674
adamnielsen7 <adam97@byui.edu>
2021-05-21 12:34:15 -0600

    tailwind testing

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function makeList(contents) {
     // add each element in contents to list
     for (i=0; i < contents.length; i++) {
         listItem = document.createElement('li');
-        listItem.innerHTML = "<a href='" + contents[i]["url"] + "'>" + contents[i]["label"] + "</a>";
+        listItem.innerHTML = "<a href='" + contents[i]["url"] + "'><span>" + contents[i]["label"] + "</span></a>";
         contentsList.appendChild(listItem);
     }
 }
